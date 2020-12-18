7a9fd48de22cb317be32049bb649c043ed123609
Abraham Cortes <74929912+azdran@users.noreply.github.com>
2020-12-18 17:26:16 -0600

    Add files via upload

---
+++ Algoritmo Shell/Algoritmo Shell/Form1.Designer.cs
@@ -0,0 +1,327 @@
+﻿namespace Algoritmo_Shell
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtLista = new System.Windows.Forms.TextBox();
+            this.btnLista = new System.Windows.Forms.Button();
+            this.ltbOrdenados = new System.Windows.Forms.ListBox();
+            this.ltbDesordenados = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnShell = new System.Windows.Forms.Button();
+            this.btnRandom = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtMayor = new System.Windows.Forms.TextBox();
+            this.txtMenor = new System.Windows.Forms.TextBox();
+            this.lbMayorR = new System.Windows.Forms.Label();
+            this.lbMenorR = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.lblTiempo = new System.Windows.Forms.Label();
+            this.lbl5 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtLista
+            // 
+            this.txtLista.Location = new System.Drawing.Point(232, 89);
+            this.txtLista.Margin = new System.Windows.Forms.Padding(2);
+            this.txtLista.Name = "txtLista";
+            this.txtLista.Size = new System.Drawing.Size(114, 20);
+            this.txtLista.TabIndex = 0;
+            // 
+            // btnLista
+            // 
+            this.btnLista.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.btnLista.Location = new System.Drawing.Point(366, 80);
+            this.btnLista.Margin = new System.Windows.Forms.Padding(2);
+            this.btnLista.Name = "btnLista";
+            this.btnLista.Size = new System.Drawing.Size(103, 37);
+            this.btnLista.TabIndex = 1;
+            this.btnLista.Text = "Agregar Lista";
+            this.btnLista.UseVisualStyleBackColor = false;
+            this.btnLista.Click += new System.EventHandler(this.btnLista_Click);
+            // 
+            // ltbOrdenados
+            // 
+            this.ltbOrdenados.FormattingEnabled = true;
+            this.ltbOrdenados.Location = new System.Drawing.Point(389, 291);
+            this.ltbOrdenados.Margin = new System.Windows.Forms.Padding(2);
+            this.ltbOrdenados.Name = "ltbOrdenados";
+            this.ltbOrdenados.Size = new System.Drawing.Size(122, 225);
+            this.ltbOrdenados.TabIndex = 4;
+            // 
+            // ltbDesordenados
+            // 
+            this.ltbDesordenados.FormattingEnabled = true;
+            this.ltbDesordenados.Location = new System.Drawing.Point(26, 297);
+            this.ltbDesordenados.Margin = new System.Windows.Forms.Padding(2);
+            this.ltbDesordenados.Name = "ltbDesordenados";
+            this.ltbDesordenados.Size = new System.Drawing.Size(116, 225);
+            this.ltbDesordenados.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(406, 261);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Ordenados";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(43, 267);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Desordenados";
+            // 
+            // btnShell
+            // 
+            this.btnShell.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.btnShell.Location = new System.Drawing.Point(216, 324);
+            this.btnShell.Margin = new System.Windows.Forms.Padding(2);
+            this.btnShell.Name = "btnShell";
+            this.btnShell.Size = new System.Drawing.Size(106, 59);
+            this.btnShell.TabIndex = 8;
+            this.btnShell.Text = "Algoritmo Shell";
+            this.btnShell.UseVisualStyleBackColor = false;
+            this.btnShell.Click += new System.EventHandler(this.btnShell_Click);
+            // 
+            // btnRandom
+            // 
+            this.btnRandom.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.btnRandom.Location = new System.Drawing.Point(359, 184);
+            this.btnRandom.Margin = new System.Windows.Forms.Padding(2);
+            this.btnRandom.Name = "btnRandom";
+            this.btnRandom.Size = new System.Drawing.Size(110, 37);
+            this.btnRandom.TabIndex = 9;
+            this.btnRandom.Text = "Agregar Random";
+            this.btnRandom.UseVisualStyleBackColor = false;
+            this.btnRandom.Click += new System.EventHandler(this.btnRandom_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(366, 17);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 37);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Agregar Tamaño";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(235, 35);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(114, 20);
+            this.textBox1.TabIndex = 11;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(248, 17);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(99, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Tamaño del Arreglo";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(233, 70);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(112, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Numeros para agregar";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtMayor);
+            this.groupBox1.Controls.Add(this.txtMenor);
+            this.groupBox1.Controls.Add(this.lbMayorR);
+            this.groupBox1.Controls.Add(this.lbMenorR);
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.btnRandom);
+            this.groupBox1.Controls.Add(this.btnLista);
+            this.groupBox1.Controls.Add(this.txtLista);
+            this.groupBox1.Location = new System.Drawing.Point(14, 13);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Size = new System.Drawing.Size(489, 246);
+            this.groupBox1.TabIndex = 14;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Arreglo";
+            // 
+            // txtMayor
+            // 
+            this.txtMayor.Location = new System.Drawing.Point(359, 144);
+            this.txtMayor.Margin = new System.Windows.Forms.Padding(2);
+            this.txtMayor.Name = "txtMayor";
+            this.txtMayor.Size = new System.Drawing.Size(114, 20);
+            this.txtMayor.TabIndex = 18;
+            // 
+            // txtMenor
+            // 
+            this.txtMenor.Location = new System.Drawing.Point(231, 145);
+            this.txtMenor.Margin = new System.Windows.Forms.Padding(2);
+            this.txtMenor.Name = "txtMenor";
+            this.txtMenor.Size = new System.Drawing.Size(114, 20);
+            this.txtMenor.TabIndex = 17;
+            // 
+            // lbMayorR
+            // 
+            this.lbMayorR.AutoSize = true;
+            this.lbMayorR.Location = new System.Drawing.Point(366, 166);
+            this.lbMayorR.Name = "lbMayorR";
+            this.lbMayorR.Size = new System.Drawing.Size(79, 13);
+            this.lbMayorR.TabIndex = 16;
+            this.lbMayorR.Text = "Mayor Random";
+            // 
+            // lbMenorR
+            // 
+            this.lbMenorR.AutoSize = true;
+            this.lbMenorR.Location = new System.Drawing.Point(251, 167);
+            this.lbMenorR.Name = "lbMenorR";
+            this.lbMenorR.Size = new System.Drawing.Size(80, 13);
+            this.lbMenorR.TabIndex = 15;
+            this.lbMenorR.Text = "Menor Random";
+            this.lbMenorR.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Algoritmo_Shell.Properties.Resources.logo_tec;
+            this.pictureBox1.Location = new System.Drawing.Point(5, 17);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(223, 224);
+            this.pictureBox1.TabIndex = 14;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(227, 420);
+            this.btnLimpiar.Margin = new System.Windows.Forms.Padding(2);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(86, 34);
+            this.btnLimpiar.TabIndex = 35;
+            this.btnLimpiar.Text = "Limpiar Datos";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // lblTiempo
+            // 
+            this.lblTiempo.AutoSize = true;
+            this.lblTiempo.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblTiempo.Location = new System.Drawing.Point(344, 546);
+            this.lblTiempo.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblTiempo.Name = "lblTiempo";
+            this.lblTiempo.Size = new System.Drawing.Size(42, 13);
+            this.lblTiempo.TabIndex = 15;
+            this.lblTiempo.Text = "Tiempo";
+            // 
+            // lbl5
+            // 
+            this.lbl5.AutoSize = true;
+            this.lbl5.Location = new System.Drawing.Point(158, 543);
+            this.lbl5.Name = "lbl5";
+            this.lbl5.Size = new System.Drawing.Size(59, 13);
+            this.lbl5.TabIndex = 36;
+            this.lbl5.Text = "Iteraciones";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.ClientSize = new System.Drawing.Size(562, 568);
+            this.Controls.Add(this.lbl5);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.lblTiempo);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnShell);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ltbDesordenados);
+            this.Controls.Add(this.ltbOrdenados);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtLista;
+        private System.Windows.Forms.Button btnLista;
+        private System.Windows.Forms.ListBox ltbOrdenados;
+        private System.Windows.Forms.ListBox ltbDesordenados;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnShell;
+        private System.Windows.Forms.Button btnRandom;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Label lblTiempo;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lbMayorR;
+        private System.Windows.Forms.Label lbMenorR;
+        private System.Windows.Forms.TextBox txtMayor;
+        private System.Windows.Forms.TextBox txtMenor;
+        private System.Windows.Forms.Label lbl5;
+    }
+}
+
---
+++ Algoritmo Shell/Algoritmo Shell/Form1.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Algoritmo_Shell
+{
+    public partial class Form1 : Form
+    {
+        Random aleatorio;
+        Shell arrayshell;
+        int[] v; // Declaracion del arreglo
+
+        DateTime inicio;
+        DateTime final;
+        TimeSpan duracion;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            arrayshell = new Shell();
+            aleatorio = new Random();
+
+            lblTiempo.Visible = false;
+            label4.Visible = false;
+            txtLista.Visible = false;
+            btnLista.Visible = false;
+
+            btnRandom.Visible = false;
+            txtMayor.Visible = false;
+            txtMenor.Visible = false;
+            lbMayorR.Visible = false;
+            lbMenorR.Visible = false;
+
+            label2.Visible = false;
+            ltbDesordenados.Visible = false;
+            label1.Visible = false;
+            ltbOrdenados.Visible = false;
+
+            btnLimpiar.Visible = false;
+            btnShell.Visible = false;
+            lbl5.Visible = false;
+
+        }
+
+        int valor, i = 0;
+
+        private void btnRandom_Click(object sender, EventArgs e)
+        {
+            ltbDesordenados.Items.Clear();//Limpia el ListBox
+
+            aleatorio = new Random();
+            int menor;
+            int mayor;
+
+            if (txtMenor.Text == "" || txtMayor.Text=="")
+            {
+                MessageBox.Show("Introduce el valor menor o mayor de los numeros a generar", "Aviso");
+                return;
+            }
+            menor = int.Parse(txtMenor.Text);
+            mayor = int.Parse(txtMayor.Text);
+
+            //ciclo utilizado para proporcionar los numeros aleatoriamente al arreglo
+            for (int i = 0; i < v.Length; i++)
+            {
+                v[i] = aleatorio.Next(menor, mayor);
+                ltbDesordenados.Items.Add(v[i]);
+            }
+
+            txtMayor.Visible = false;
+            txtMenor.Visible = false;
+            lbMayorR.Visible = false;
+            lbMenorR.Visible = false;
+
+            label4.Visible = false;
+            txtLista.Visible = false;
+            btnLista.Visible = false;
+            btnRandom.Visible = false;
+
+            label2.Visible = true;
+            ltbDesordenados.Visible = true;
+            label1.Visible = true;
+            ltbOrdenados.Visible = true;
+
+            btnLimpiar.Visible = true;
+            btnShell.Visible = true;
+            lbl5.Visible = true;
+
+            txtMenor.Clear();
+            txtMayor.Clear();
+        }
+
+        private void btnShell_Click(object sender, EventArgs e)
+        {
+            inicio = DateTime.Now;
+            arrayshell.MetodoShell(v);
+            final = DateTime.Now;
+            duracion = final - inicio;
+            lblTiempo.Text = "Tiempo Transcurrido: " + duracion;
+            lbl5.Text = "Iteraciones: " + arrayshell.MetodoShell(v);
+
+            //agrega los valores ya ordenados a nuestro vector
+            for (int i = 0; i < v.Length; i++)
+            {
+                ltbOrdenados.Items.Add(v[i]);
+
+            }
+            lblTiempo.Visible = true;
+            btnShell.Visible = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+            {
+                MessageBox.Show("Introducir el tamaño del Arreglo","Aviso");
+                return;
+            }
+            
+            v = new int[int.Parse(textBox1.Text)]; //Tamaño del arreglo
+            ltbDesordenados.Items.Clear();
+
+            txtMayor.Visible = true;
+            txtMenor.Visible = true;
+            lbMayorR.Visible = true;
+            lbMenorR.Visible = true;
+
+            label3.Visible = false;
+            textBox1.Visible = false;
+            button1.Visible = false;
+
+            label4.Visible = true;
+            txtLista.Visible = true;
+            btnLista.Visible = true;
+
+            btnRandom.Visible = true;
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            ltbDesordenados.Items.Clear();
+            ltbOrdenados.Items.Clear();
+            lbl5.Visible = false;
+            textBox1.Clear();
+
+            lblTiempo.Visible = false;
+            label4.Visible = false;
+            txtLista.Visible = false;
+            btnLista.Visible = false;
+
+            btnRandom.Visible = false;
+
+            label2.Visible = false;
+            ltbDesordenados.Visible = false;
+            label1.Visible = false;
+            ltbOrdenados.Visible = false;
+
+            btnLimpiar.Visible = false;
+            btnShell.Visible = false;
+
+            label3.Visible = true;
+            textBox1.Visible = true;
+            button1.Visible = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLista_Click(object sender, EventArgs e)
+        {
+            if (txtLista.Text == "")
+            {
+                MessageBox.Show("Ingrese los Valores");
+                return;
+            }
+            valor = int.Parse(txtLista.Text);
+            v[i++] = valor;
+            ltbDesordenados.Items.Add(v[i - 1]);
+            txtLista.Clear();
+            txtLista.Focus();
+
+            btnRandom.Visible = false;
+
+            label2.Visible = true;
+            ltbDesordenados.Visible = true;
+            label1.Visible = true;
+            ltbOrdenados.Visible = true;
+
+            btnLimpiar.Visible = true;
+            btnShell.Visible = true;
+        }
+    }
+}
